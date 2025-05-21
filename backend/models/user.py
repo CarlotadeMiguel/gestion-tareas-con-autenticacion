7e419a8b1eae663a06a3a58b0c1a1ce304b348ab
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-21 17:51:29 +0200

    añadir  hash

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,7 @@
+from werkzeug.security import generate_password_hash
+
 class User:
     def __init__(self, username, password):
         self.username = username
-        self.password = password  
+        self.password = generate_password_hash(password)
         self.tasks = []
