d69789b37ce08194ecd5853f625a0c29870cfff3
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-21 18:29:49 +0200

    Iniciar jwt

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,16 @@ from flask_cors import CORS
 from config import *
 from routes.auth_routes import auth_bp
 from routes.task_routes import task_bp
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config.from_object('config')
 CORS(app)
+jwt = JWTManager(app)
 
 # Registra tus blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(task_bp)
 
 if __name__ == "__main__":
-    app.run(ssl_context=SSL_CONTEXT)
+    app.run(debug=True)
