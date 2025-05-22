b1d4e0b7ef343f9e9d1b02b7cb40a3f6586d886c
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-22 20:07:46 +0200

    añadir post

--- frontend/app/api/tasks/route.ts
+++ frontend/app/api/tasks/route.ts
@@ -2,25 +2,56 @@ import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
 export async function GET() {
-    const cookieStore = await cookies();
-    const token = cookieStore.get('token');
+  const cookieStore = cookies();
+  const token = cookieStore.get('token')?.value;
 
-  try {
-    const response = await fetch('http://localhost:5000/api/tasks', {
-      method: 'GET',
-      headers: { Authorization: `Bearer ${token}` },
-      cache: 'no-store',
-    });
-
-    const data = await response.json();
-
-    if (!response.ok) {
-      return NextResponse.json({ error: data.message }, { status: response.status });
-    }
-
-    return NextResponse.json( data );
-  } catch (error) {
-    console.error('Error en login:', error);
-    return NextResponse.json({ error: 'Error al procesar la solicitud' }, { status: 500 });
+  if (!token) {
+    return NextResponse.json({ error: 'No token found' }, { status: 401 });
   }
+
+  const response = await fetch('http://localhost:5000/api/tasks', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    return NextResponse.json(data, { status: response.status });
+  }
+
+  return NextResponse.json(data);
 }
+
+
+export async function POST(request: Request) {
+    const body = await request.json();
+    const { title, description } = body;
+  
+    const cookieStore = cookies();
+    const token = cookieStore.get('token')?.value;
+  
+    if (!token) {
+      return NextResponse.json({ error: 'No token found' }, { status: 401 });
+    }
+  
+    const response = await fetch('http://localhost:5000/api/tasks', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      cache: 'no-store',
+      body : JSON.stringify({ title, description }),
+    });
+    
+    if (!response.ok) {
+      return NextResponse.json(response, { status: response.status });
+    }
+  
+    return NextResponse.json({ success: true });
+  }
