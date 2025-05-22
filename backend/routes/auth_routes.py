9c5a31e6dd448fa05071ea4120100e34e7fb9ae6
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-22 19:01:03 +0200

    añadir token a las cookeis

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, make_response, request, jsonify
 from flask_jwt_extended import create_access_token
 from managers.auth_manager import AuthManager
 
@@ -14,7 +14,9 @@ def register():
     try:
         AuthManager.register(username, password)
         access_token = create_access_token(identity=username)
-        return jsonify({"token": access_token}), 201
+        resp = make_response(jsonify({"msg": "login successful"}), 200)
+        resp.set_cookie("token", access_token, httponly=True, samesite='Lax')
+        return resp
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
 
@@ -28,6 +30,8 @@ def login():
     try:
         user = AuthManager.verify_credentials(username, password)
         access_token = create_access_token(identity=user.username)
-        return jsonify({"token": access_token}), 200
+        resp = make_response(jsonify({"msg": "login successful"}), 200)
+        resp.set_cookie("token", access_token, httponly=True, samesite='Lax')
+        return resp
     except ValueError as e:
         return jsonify({"message": str(e)}), 401
