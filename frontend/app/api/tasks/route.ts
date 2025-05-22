048c0ba56d10988793c52bed5d61db57a8be618a
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-22 18:38:46 +0200

    add route de las task

---
+++ frontend/app/api/tasks/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+export async function GET() {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token');
+
+  try {
+    const response = await fetch('http://localhost:5000/api/tasks', {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` },
+      cache: 'no-store',
+      credentials: 'include', // Para incluir cookies
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return NextResponse.json({ error: data.message }, { status: response.status });
+    }
+
+    return NextResponse.json({ data });
+  } catch (error) {
+    console.error('Error en login:', error);
+    return NextResponse.json({ error: 'Error al procesar la solicitud' }, { status: 500 });
+  }
+}
