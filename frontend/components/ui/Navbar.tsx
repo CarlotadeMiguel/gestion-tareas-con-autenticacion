8faa098c970f3116af81d2297a69ac2bd9d9d132
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-22 18:08:15 +0200

    añadir el cambio de tema

--- frontend/components/ui/Navbar.tsx
+++ frontend/components/ui/Navbar.tsx
@@ -1,11 +1,13 @@
 'use client';
 import { useAuth } from '@/context/AuthContext';
+import ThemeToggle from './ThemeToggle';
 
 export default function Navbar() {
   const { logout, isAuthenticated } = useAuth();
 
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center">
+      <ThemeToggle />
           <button
             onClick={logout}
             className="ml-4 bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700"
