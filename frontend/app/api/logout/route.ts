d27366e9d4cc2440bade972baae0d3ece4dca5fd
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-05-23 18:14:54 +0200

    solucionar error

--- frontend/app/api/logout/route.ts
+++ frontend/app/api/logout/route.ts
@@ -2,13 +2,13 @@ import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
 export async function POST() {
- 
-  cookies().set('token', '', {
+  const cookieStore = await cookies();
+  cookieStore.set('token', '', {
     httpOnly: true,
     path: '/',
-    expires: new Date(0), 
-    sameSite: 'lax', 
-    secure: true,     
+    expires: new Date(0),
+    sameSite: 'lax',
+    secure: true,
   });
 
   return NextResponse.json({ message: 'Sesión cerrada' }, { status: 200 });
